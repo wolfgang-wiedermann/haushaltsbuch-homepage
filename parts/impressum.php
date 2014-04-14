904626d119dae1cee1d74d346cdb00e0f15303c2
Wolfgang Wiedermann <wolfgang.wiedermann@hs-regensburg.de>
2014-04-14 14:36:00 +0200

    Copyright-Hinweise für Bootstrap und Datenschutzhinweis für Google Analytics hinzugefügt (und Analytics natürlich selbst)

--- parts/impressum.php
+++ parts/impressum.php
@@ -11,6 +11,23 @@
     E-Mail: <img src="./img/impressum_email.png"/>
     </p>
 </div>
+<div>
+<h2>Datenschutzerklärung zu Google Analytics</h2>
+<p>
+Diese Website benutzt Google Analytics, einen Webanalysedienst der Google Inc. („Google“). Google Analytics verwendet sog. „Cookies“, Textdateien, die auf Ihrem Computer gespeichert werden und die eine Analyse der Benutzung der Website durch Sie ermöglichen. Die durch den Cookie erzeugten Informationen über Ihre Benutzung dieser Website werden in der Regel an einen Server von Google in den USA übertragen und dort gespeichert. Im Falle der Aktivierung der IP-Anonymisierung auf dieser Website, wird Ihre IP-Adresse von Google jedoch innerhalb von Mitgliedstaaten der Europäischen Union oder in anderen Vertragsstaaten des Abkommens über den Europäischen Wirtschaftsraum zuvor gekürzt. Nur in Ausnahmefällen wird die volle IP-Adresse an einen Server von Google in den USA übertragen und dort gekürzt. Im Auftrag des Betreibers dieser Website wird Google diese Informationen benutzen, um Ihre Nutzung der Website auszuwerten, um Reports über die Websiteaktivitäten zusammenzustellen und um weitere mit der Websitenutzung und der Internetnutzung verbundene Dienstleistungen gegenüber dem Websitebetreiber zu erbringen. Die im Rahmen von Google Analytics von Ihrem Browser übermittelte IP-Adresse wird nicht mit anderen Daten von Google zusammengeführt. Sie können die Speicherung der Cookies durch eine entsprechende Einstellung Ihrer Browser-Software verhindern; wir weisen Sie jedoch darauf hin, dass Sie in diesem Fall gegebenenfalls nicht sämtliche Funktionen dieser Website vollumfänglich werden nutzen können. Sie können darüber hinaus die Erfassung der durch das Cookie erzeugten und auf Ihre Nutzung der Website bezogenen Daten (inkl. Ihrer IP-Adresse) an Google sowie die Verarbeitung dieser Daten durch Google verhindern, indem sie das unter dem folgenden Link (<a href="http://tools.google.com/dlpage/gaoptout?hl=de">http://tools.google.com/dlpage/gaoptout?hl=de</a>) verfügbare Browser-Plugin herunterladen und installieren. <br>
+<br>
+Weitere Informationen zu den Nutzungsbedingungen von Google Analytics finden Sie unter <a href="http://www.google.com/analytics/terms/de.html">http://www.google.com/analytics/terms/de.html</a>
+</p>
+</div>
+<div>
+<h2></h2>
+</div>
+<h2>Copyright Hinweise zu verwendeter Software</h2>
+Diese Seite basiert auf dem Web-Framework Bootstrap:
+Bootstrap is released under the MIT license and its copyright 2014 belongs to Twitter.<br/>
+<br>
+Das HTML5-Haushaltsbuch verwendet JQuery und JQuery Mobile:
+beide stehen ebenfalls unter MIT License (siehe <a href="https://jquery.org/license/">https://jquery.org/license/</a>)
 </div>
 </section>
 <!-- Impressum des Haushaltsbuch Ende -->

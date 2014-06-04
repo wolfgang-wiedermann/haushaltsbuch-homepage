d1dbc87064f1a5bbdce4675d7a55060fa2e1bfab
Wolfgang Wiedermann <wolfgang.wiedermann@hs-regensburg.de>
2014-06-04 19:12:12 +0200

    Ankündigung des nächsten Releases eingefügt

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include("header.php");
 include("parts/menu.php");
 // Seiteninhalt 
 include("parts/jumbotron.php");
-#include("parts/carousel.php");
+include("parts/ankuendigung.php");
 #include("parts/nutzen.php");
 include("parts/screenshots.php");
 include("parts/geschichte.php");
---
+++ parts/ankuendigung.php
@@ -0,0 +1,17 @@
+<!-- Ankuendigung -->
+<div class="container" id="ankuendigung_dialog">
+<!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron" itemprop="description">
+        <h1>Release-Ankündigung</h1>
+        <p>
+        Am 01.07.2014 erscheint das Release 0.1.04 des HTML5-Haushaltsbuchs mit
+        </p> 
+        <ul style="font-size:16pt;">
+        <li>einem Installations-Wizzard,</li>
+        <li>neuen Auswertungsoptionen,</li>
+        <li>verbesserter Verwaltung der Schnellbuchungsvorlagen,</li>
+        <li>und verschiedenen Detail-Verbesserungen.</li>
+        </ul>
+    </div>
+</div>
+<!-- Ankuendigung Ende -->

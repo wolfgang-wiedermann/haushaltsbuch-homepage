20116149b2c5bba9e0ce38a8bf111736c342a4c9
Wolfgang Wiedermann <wolfgang.wiedermann@hs-regensburg.de>
2014-06-08 15:53:08 +0200

    Version richtiggestellt und einen Verweis auf das Changelog eingefügt

--- parts/ankuendigung.php
+++ parts/ankuendigung.php
@@ -4,7 +4,7 @@
     <div class="jumbotron" itemprop="description">
         <h1>Release-Ankündigung</h1>
         <p>
-        Am 01.07.2014 erscheint das Release 0.1.04 des HTML5-Haushaltsbuchs mit
+        Am 01.07.2014 erscheint das Release 0.1.03 des HTML5-Haushaltsbuchs mit
         </p> 
         <ul style="font-size:16pt;">
         <li>einem Installations-Wizzard,</li>
@@ -12,6 +12,8 @@
         <li>verbesserter Verwaltung der Schnellbuchungsvorlagen,</li>
         <li>und verschiedenen Detail-Verbesserungen.</li>
         </ul>
+        <p>Eine detaillierte Auflistung der &Auml;nderungen bzw. Neuerungen finden Sie im
+        <a href="https://github.com/wolfgang-wiedermann/php_mobile_accounting/compare/v0.1.02...715495fa5260176797e4d164878ec7608e42dfc9">Changelog</a> auf Github.
     </div>
 </div>
 <!-- Ankuendigung Ende -->

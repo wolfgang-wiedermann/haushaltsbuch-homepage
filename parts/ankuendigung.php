8a49cdfe333b9c145dc02eb98bb464051b02c749
Wolfgang Wiedermann <wolfgang.wiedermann@hs-regensburg.de>
2014-07-05 22:06:21 +0200

    Text der Ankündigung angepasst

--- parts/ankuendigung.php
+++ parts/ankuendigung.php
@@ -2,9 +2,9 @@
 <div class="container" id="ankuendigung_dialog">
 <!-- Main jumbotron for a primary marketing message or call to action -->
     <div class="jumbotron" itemprop="description">
-        <h1>Release-Ankündigung</h1>
+        <h1>Release 0.1.03 erschienen</h1>
         <p>
-        Am 01.07.2014 erscheint das Release 0.1.03 des HTML5-Haushaltsbuchs mit
+        Am 01.07.2014 ist das Release 0.1.03 des HTML5-Haushaltsbuchs erschienen. Es enthält unter Anderem
         </p> 
         <ul style="font-size:16pt;">
         <li>einem Installations-Wizzard,</li>
@@ -13,7 +13,9 @@
         <li>und verschiedenen Detail-Verbesserungen.</li>
         </ul>
         <p>Eine detaillierte Auflistung der &Auml;nderungen bzw. Neuerungen finden Sie im
-        <a href="https://github.com/wolfgang-wiedermann/php_mobile_accounting/compare/v0.1.02...715495fa5260176797e4d164878ec7608e42dfc9">Changelog</a> auf Github.
+        <a href="https://github.com/wolfgang-wiedermann/php_mobile_accounting/compare/v0.1.02...715495fa5260176797e4d164878ec7608e42dfc9">Changelog</a> auf Github.<br/>
+<br/>
+Seit dem 05.07.2014 ist auch das Demosystem auf Release 0.1.03 aktualisiert.
     </div>
 </div>
 <!-- Ankuendigung Ende -->

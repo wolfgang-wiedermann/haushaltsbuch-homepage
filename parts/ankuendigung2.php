d9a643e08ce14a49322397f03c800c8e8538cadf
Wolfgang Wiedermann <wolfgang.wiedermann@hs-regensburg.de>
2015-01-12 18:51:54 +0100

    Release 0.1.04 angekündigt

---
+++ parts/ankuendigung2.php
@@ -0,0 +1,23 @@
+<!-- Ankuendigung -->
+<div class="container" id="ankuendigung_dialog">
+<!-- Main jumbotron for a primary marketing message or call to action -->
+    <div class="jumbotron" itemprop="description">
+        <h1>Ankündigung</h1>
+        <p>
+        Am 01.02.2015 wird das Release 0.1.04 des HTML5-Haushaltsbuchs erscheinen. Es enthält unter Anderem
+        </p> 
+        <ul style="font-size:16pt;">
+        <li>eine verbesserte Darstellung der Gewinn und Verlust-Rechnung auf Jahresebene,</li>
+        <li>neue Auswertungsoptionen zur Visualisierung des zeitlichen Verlaufs,</li>
+        <li>eine Export-Funktionen zur Datenanalyse mit Microsoft Excel oder LiberOffice Calc,</li>
+        <li>einer Voransicht getätigter, noch nicht übertragener Offline-Buchungen,</li>
+        <li>und verschiedene Detail-Verbesserungen.</li>
+        </ul>
+        <p>Eine detaillierte Auflistung der &Auml;nderungen bzw. Neuerungen finden Sie im
+        <a href="https://github.com/wolfgang-wiedermann/php_mobile_accounting/compare/v0.1.03...05c5a3073c33f835ade0e5932f27a456b60f848d">Changelog</a> auf Github.<br/>
+<br/>
+Das derzeitige Demosystem basiert noch auf dem alten Release 0.1.03. Eine aktualisierung ist für den Erscheinungszeitpunkt 
+des neuen Releases geplant.
+    </div>
+</div>
+<!-- Ankuendigung Ende -->

dd06600a07dae5eb311926c8447b10979af94c96
Wolfgang Wiedermann <wolfgang.wiedermann@hs-regensburg.de>
2015-01-16 23:15:20 +0100

    Anleitung für den Export nach LibreOffice Calc eingebaut; Vergessene Datei hinzugefügt

---
+++ export_howto.php
@@ -0,0 +1,11 @@
+<?php
+// Kopfbereich
+include("header.php");
+// Menüleiste
+include("parts/menu.php");
+// Seiteninhalt 
+include("parts/export_howto.php");
+// Ende Seiteninhalt
+// Fußbereich
+include("footer.php");
+?>

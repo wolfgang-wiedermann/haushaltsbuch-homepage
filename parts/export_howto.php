e6deb246a3bedb39df9e16d30478d1b8539e0b37
Wolfgang Wiedermann <wolfgang.wiedermann@hs-regensburg.de>
2015-01-24 13:39:15 +0100

    Details nachgebessert für die Seite 'Auswerten mit LibreOffice'

--- parts/export_howto.php
+++ parts/export_howto.php
@@ -23,12 +23,14 @@
     <img src="./img/export_howto_auswertung.png">
     <br/><br/>
     <li>Öffnen Sie die heruntergeladene Datei mit Doppelklick mit Excel oder Calc</li>
+    <h3>Detaildaten der Gewinn und Verlust-Rechnung nach LibreOffice Calc importieren</h3>
     <li>Beim öffnen mit Calc öffnet sich dann der folgende Datenimport-Dialog</li>
     <img src="./img/export_howto_importieren.png">
     <br/><br/>
     <li>Das Ergebnis des Imports ist eine Tabelle der Monatssalden der Ertrags- und Aufwandskonten.</li>
     <img src="./img/export_howto_importiert.png">
-    <br/><br/>
+    <br/>
+    <h3>Als Pivot-Tabelle in LibreOffice Calc auswerten</h3>
     <p>
     Die so importierten Daten eignen sich besonders gut für eine Auswertung als Pivot-Tabelle. Dazu gehen Sie wie folgt vor.
     </p>
@@ -42,5 +44,6 @@
     <br/><br/>
     <img src="./img/export_howto_pivot_ergebnis.png">
     </ol>
+    Entsprechende Screenshots für den Import in Microsoft Excel werden demnächst zur Verfügung gestellt.
     </div>
 </div>
